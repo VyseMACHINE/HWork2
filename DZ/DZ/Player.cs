4b4c739581121d6749c0736a8c33aa00fefe90fd
VyseMACHINE <40603631+VyseMACHINE@users.noreply.github.com>
2019-01-23 20:59:18 +0600

    Add files via upload

---
+++ DZ/DZ/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DZ
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ DZ/DZ/Sprint.cs
@@ -0,0 +1,15 @@
+﻿namespace DZ
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class Sprint : DbContext
+    {
+        public Sprint()
+            : base("name=Sprint")
+        {
+        }
+        public DbSet<Player> Players { get; set; }
+    }
+}
